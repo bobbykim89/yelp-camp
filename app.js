c6785530ebaa64b8971bbe449c9df68a13068413
bobbykim89 <bobby.sihun.kim@gmail.com>
2021-08-01 19:49:23 -0700

    updated secirity for deployment

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    //secure: true, // Comment this out on dev mode
+    secure: true, // Comment this out on dev mode
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
